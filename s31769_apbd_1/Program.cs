e82e0db6916ab18f645a214a94a56baee511e23f
mekh <smajev@protonmail.com>
2026-03-26 18:41:39 +0100

    Initial project setup

--- s31769_apbd_1/Program.cs
+++ s31769_apbd_1/Program.cs
@@ -1,16 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using DefaultNamespace;
-
-LoanService loanService = new LoanService();
-
-var student = new Student() { FirstName = "Jan", LastName = "Kowalski" };
-var laptop = new Laptop { Name = "Dell XPS", RamGb = 16 };
-
-
-loanService.Borrow(student, laptop);
-
-Console.WriteLine(loanService._loans[0].IsReturned);
-// var penalty = loanService.Return(loan);
-
 Console.WriteLine("Hello, World!");
