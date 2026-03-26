aad86014474b81ecf5d5c94c1fa8536c39200da3
mekh <smajev@protonmail.com>
2026-03-26 19:05:07 +0100

    Add equipment

--- s31769_apbd_1/Models/equipment/Equipment.cs
+++ s31769_apbd_1/Models/equipment/Equipment.cs
@@ -5,4 +5,5 @@ public class Equipment
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; }
     public EquipmentStatus Status { get; set; } = EquipmentStatus.Available;
+    
 }
